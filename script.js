b2c6469ed745becf49b982ab974296b1c235431b
Priyansh Dimri <priyanshd0908@gmail.com>
2024-10-13 21:24:51 +0530

    feat: add option number validation

--- script.js
+++ script.js
@@ -6,12 +6,9 @@ console.info(`Please choose one of the following option number when prompted:
     3. Scissors`)
 console.log("Type play() in console to start the game!");
 
-function play() {
-    
-    let a = 10;
-    
+function play() {    
     // Continuously loop the game for user's input until user types 'q'
-    while (a--) {
+    while (true) {
         // Ask user to enter 1, 2 or 3 where 1 means rock, 2 means paper, 3 means scissors or 'q' for quit
         console.log(`Please choose one of the following option number:
             1. Rock
@@ -21,13 +18,21 @@ function play() {
         let option = prompt("Enter option(q for quit)");
         
         if(option === 'q') break;
+        
+        let option_number = parseInt(option);
+
+        // Check if the entered input is valid or not
+        if(option_number === NaN || option_number < 0 || option_number > 3) {
+            // If it is invalid, provide a warning telling the user to input proper input and loop again
+            console.warn("Please enter a valid option number or 'q' for quit!");
+            continue;
+        }
     
+        // If it is valid, use a random number out of 1, 2 or 3 by the computer
         let computer_option = Math.floor(Math.random() * 4);
         console.log(computer_option);
-    
-        // Check if the entered input is valid or not
-        // If it is invalid, provide a warning telling the user to input proper input and loop again
-        // If it is valid, use a random number out of 1, 2 or 3 by the computer
+
+        let winner = "Tie";
         // Check the following:
         // User Computer Winner
         // 1    1        Tie

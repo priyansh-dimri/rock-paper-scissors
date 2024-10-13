f91a68ddee6bde3bb5583b764feab917c424e478
Priyansh Dimri <priyanshd0908@gmail.com>
2024-10-13 17:35:36 +0530

    feat: add user input feature

--- script.js
+++ script.js
@@ -2,19 +2,32 @@
 console.log("Welcome to Rock Paper Scissors Game!");
 
 // Continuously loop the game for user's input until user types 'q'
-// Ask user to enter 1, 2 or 3 where 1 means rock, 2 means paper, 3 means scissors or 'q' for quit
-// Check if the entered input is valid or not
-// If it is invalid, provide a warning telling the user to input proper input and loop again
-// If it is valid, use a random number out of 1, 2 or 3 by the computer
-// Check the following:
-// User Computer Winner
-// 1    1        Tie
-// 1    2        Computer
-// 1    3        User
-// 2    1        User
-// 2    2        Tie
-// 2    3        Computer
-// 3    1        Computer
-// 3    2        User
-// 3    3        Tie
-// Print the result as console.info and loop continues
+while (true) {
+    // Ask user to enter 1, 2 or 3 where 1 means rock, 2 means paper, 3 means scissors or 'q' for quit
+    console.log(`Please choose one of the following option number:
+        1. Rock
+        2. Paper
+        3. Scissors`)
+    
+    let option = prompt("Enter option(q for quit): ");
+    
+    if(option === 'q') break;
+
+    // Check if the entered input is valid or not
+    // If it is invalid, provide a warning telling the user to input proper input and loop again
+    // If it is valid, use a random number out of 1, 2 or 3 by the computer
+    // Check the following:
+    // User Computer Winner
+    // 1    1        Tie
+    // 1    2        Computer
+    // 1    3        User
+    // 2    1        User
+    // 2    2        Tie
+    // 2    3        Computer
+    // 3    1        Computer
+    // 3    2        User
+    // 3    3        Tie
+    // Print the result as console.info and loop continues
+}
+
+console.log("Thank you for playing!");
